c9bba7b09519ebed163cda24cd0ffbbb95072803
Vignesh <joshivignesh@users.noreply.github.com>
2023-08-16 22:43:59 +0530

    Added Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TaskdataService } from './taskdata/taskdata.service';
 
 @Module({
   imports: [TasksModule],
-  providers: [],
+  providers: [TaskdataService],
 })
 export class AppModule {}
