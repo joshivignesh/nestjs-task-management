3cd8c610af6d6f9b3b264284c24947cc516e1baa
Vignesh <joshivignesh@users.noreply.github.com>
2023-08-21 23:35:09 +0530

    Added new Dto and new methods in Controler

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,10 +6,13 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-task-filter.dto';
+import { filter } from 'rxjs';
 
 @Controller('tasks')
 export class TasksController {
@@ -18,6 +21,36 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    // if we have any filters defined, call taskService.getTaskwithFilters
+    // otherwise, just get all the tasks
+    if (Object.keys(filterDto).length) {
+      //
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    // define a temporary array to hold result
+    let tasks = this.getAllTasks();
+
+    // do something with status
+    if (status) {
+      tasks = tasks.filter((task) => task.status == status);
+    }
+
+    // do something with search
+    if (search) {
+      tasks = tasks.filter((task) => task.title == search);
+    }
+    // return final result
+    return tasks;
   }
 
   @Get('/:id')
