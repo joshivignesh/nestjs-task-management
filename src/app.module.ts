b927412aa45353a748a87716c39d4a3d44fb1324
Vignesh <joshivignesh@users.noreply.github.com>
2023-08-27 03:10:06 +0530

    Installed required packages and  added Connection to Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TaskdataService } from './taskdata/taskdata.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
-  providers: [TaskdataService],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  providers: [],
 })
 export class AppModule {}
